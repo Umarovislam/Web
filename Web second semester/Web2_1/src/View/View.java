d9ed355c6cda4ee344e6ee0bdcedab19618429af
Islam Umarov <fpm.umarov@bsu.by>
2019-03-01 21:33:00 +0300

    rewriting hole project

--- Web second semester/Web2_1/src/View/View.java
+++ Web second semester/Web2_1/src/View/View.java
@@ -1,11 +1,29 @@
 package View;
 
-import Connection.Connections;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
 public class View {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException {
+        Properties props = new Properties();
+        FileInputStream in = new FileInputStream("D:\\3 course\\Web\\Web second semester\\Web2_1\\src\\database.properties");
+        props.load(in);
+        in.close();
 
-    public static void main(String[] args) throws Exception {
-        Connections cn = new Connections();
-        System.out.println(cn.getConnections());
+        String driver = props.getProperty("driver");
+        if (driver != null) {
+            Class.forName(driver) ;
+        }
+
+        String url = props.getProperty("url");
+        String username = props.getProperty("login");
+        String password = props.getProperty("password");
+
+        Connection con = DriverManager.getConnection(url, username, password);
     }
 }
