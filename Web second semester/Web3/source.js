51a66689e8bc5a4e216d80cd91f88d2a84cbb737
Islam <UmarovIslam>
2019-05-03 01:25:11 +0300

    added 3rd lab

---
+++ Web second semester/Web3/source.js
@@ -0,0 +1,822 @@
+var c = [
+    {label: 'btCheck', elemtype: 'button', name: 'btChecks'},
+    {label:'it', name:'inputCh', elemtype:'input',className:'tbCh'},
+    {label:'chb', name:'chb', elemtype:'checkbox',className:'cbCh'}
+];
+
+objs = [
+    {
+        "Rank": 1,
+        "User": "Липатов Александр",
+        "Status": "Finished",
+        "Steps": 66683
+    },
+    {
+        "Rank": 2,
+        "User": "Косинов Павел",
+        "Status": "Finished",
+        "Steps": 41389
+    },
+    {
+        "Rank": 3,
+        "User": "Разпустов Николай",
+        "Status": "Finished",
+        "Steps": 41197
+    },
+    {
+        "Rank": 4,
+        "User": "Убывающая Женя",
+        "Status": "Finished",
+        "Steps": 40000
+    },
+    {
+        "Rank": 5,
+        "User": "Караваев Святослав",
+        "Status": "Finished",
+        "Steps": 39327
+    },
+    {
+        "Rank": 6,
+        "User": "Юмашев Леонид",
+        "Status": "Finished",
+        "Steps": 39295
+    },
+    {
+        "Rank": 7,
+        "User": "Разуваев Виктор",
+        "Status": "Finished",
+        "Steps": 39153
+    },
+    {
+        "Rank": 8,
+        "User": "Оченьбольшой Федор",
+        "Status": "Finished",
+        "Steps": 38849
+    },
+    {
+        "Rank": 9,
+        "User": "Капустин Назар",
+        "Status": "Finished",
+        "Steps": 38467
+    },
+    {
+        "Rank": 10,
+        "User": "Ткаченко Пелагея",
+        "Status": "Finished",
+        "Steps": 38180
+    },
+    {
+        "Rank": 11,
+        "User": "Кулатова Влада",
+        "Status": "Finished",
+        "Steps": 37479
+    },
+    {
+        "Rank": 12,
+        "User": "Белоусова Алиса",
+        "Status": "Finished",
+        "Steps": 36947
+    },
+    {
+        "Rank": 13,
+        "User": "Безбородов Адам",
+        "Status": "Finished",
+        "Steps": 36275
+    },
+    {
+        "Rank": 14,
+        "User": "Мазурин Терентий",
+        "Status": "Finished",
+        "Steps": 35985
+    },
+    {
+        "Rank": 15,
+        "User": "Турчанинов Лев",
+        "Status": "Finished",
+        "Steps": 32907
+    },
+    {
+        "Rank": 16,
+        "User": "Катериночкина Дарья",
+        "Status": "Finished",
+        "Steps": 32581
+    },
+    {
+        "Rank": 17,
+        "User": "Якушев Вениамин",
+        "Status": "Finished",
+        "Steps": 32543
+    },
+    {
+        "Rank": 18,
+        "User": "Тринуликов Сергей",
+        "Status": "Finished",
+        "Steps": 32083
+    },
+    {
+        "Rank": 19,
+        "User": "Пименов Александр",
+        "Status": "Finished",
+        "Steps": 30791
+    },
+    {
+        "Rank": 20,
+        "User": "Муленко Агния",
+        "Status": "Finished",
+        "Steps": 30572
+    },
+    {
+        "Rank": 21,
+        "User": "Янченко Владлен",
+        "Status": "Finished",
+        "Steps": 30009
+    },
+    {
+        "Rank": 22,
+        "User": "Еркулаев Рубен",
+        "Status": "Finished",
+        "Steps": 29854
+    },
+    {
+        "Rank": 23,
+        "User": "Лассман Петр",
+        "Status": "Finished",
+        "Steps": 29377
+    },
+    {
+        "Rank": 24,
+        "User": "Толстой Жанна",
+        "Status": "Finished",
+        "Steps": 29124
+    },
+    {
+        "Rank": 25,
+        "User": "Северинов Фока",
+        "Status": "Finished",
+        "Steps": 28974
+    },
+    {
+        "Rank": 26,
+        "User": "Кадцын Герман",
+        "Status": "Finished",
+        "Steps": 28812
+    },
+    {
+        "Rank": 27,
+        "User": "Зефирова Майя",
+        "Status": "Finished",
+        "Steps": 28308
+    },
+    {
+        "Rank": 28,
+        "User": "Мухова Эмма",
+        "Status": "Finished",
+        "Steps": 27159
+    },
+    {
+        "Rank": 29,
+        "User": "Куксилова Ольга",
+        "Status": "Finished",
+        "Steps": 26904
+    },
+    {
+        "Rank": 30,
+        "User": "Базарова Зинаида",
+        "Status": "Finished",
+        "Steps": 25349
+    },
+    {
+        "Rank": 31,
+        "User": "Менде Артём",
+        "Status": "Finished",
+        "Steps": 24667
+    },
+    {
+        "Rank": 32,
+        "User": "﻿Александрова Татьяна",
+        "Status": "Finished",
+        "Steps": 24566
+    },
+    {
+        "Rank": 33,
+        "User": "Горохов Харитон",
+        "Status": "Finished",
+        "Steps": 24563
+    },
+    {
+        "Rank": 34,
+        "User": "Колвецкий Фока",
+        "Status": "Finished",
+        "Steps": 22834
+    },
+    {
+        "Rank": 35,
+        "User": "Яблочкин Валерьян",
+        "Status": "Finished",
+        "Steps": 22805
+    },
+    {
+        "Rank": 36,
+        "User": "Паршиков Лавр",
+        "Status": "Finished",
+        "Steps": 22002
+    },
+    {
+        "Rank": 37,
+        "User": "Веселовский Фока",
+        "Status": "Finished",
+        "Steps": 21351
+    },
+    {
+        "Rank": 38,
+        "User": "Сорокина Жанна",
+        "Status": "Finished",
+        "Steps": 20873
+    },
+    {
+        "Rank": 39,
+        "User": "Худовекова Агния",
+        "Status": "Finished",
+        "Steps": 20666
+    },
+    {
+        "Rank": 40,
+        "User": "Коршиков Платон",
+        "Status": "Finished",
+        "Steps": 20661
+    },
+    {
+        "Rank": 41,
+        "User": "Порсев Мартын",
+        "Status": "Finished",
+        "Steps": 20263
+    },
+    {
+        "Rank": 42,
+        "User": "Ядрищенский Станислав",
+        "Status": "Finished",
+        "Steps": 20131
+    },
+    {
+        "Rank": 43,
+        "User": "Рабиновича Регина",
+        "Status": "Finished",
+        "Steps": 19674
+    },
+    {
+        "Rank": 44,
+        "User": "Худовекова Марина",
+        "Status": "Finished",
+        "Steps": 19514
+    },
+    {
+        "Rank": 45,
+        "User": "Дёшина Ева",
+        "Status": "Finished",
+        "Steps": 19391
+    },
+    {
+        "Rank": 46,
+        "User": "Ноздрёва Жанна",
+        "Status": "Finished",
+        "Steps": 19170
+    },
+    {
+        "Rank": 47,
+        "User": "Скотарев Никон",
+        "Status": "Finished",
+        "Steps": 18786
+    },
+    {
+        "Rank": 48,
+        "User": "Ревягин Варфоломей",
+        "Status": "Finished",
+        "Steps": 18285
+    },
+    {
+        "Rank": 49,
+        "User": "Гончарова Алина",
+        "Status": "Finished",
+        "Steps": 18190
+    },
+    {
+        "Rank": 50,
+        "User": "Норин Никита",
+        "Status": "Finished",
+        "Steps": 18132
+    },
+    {
+        "Rank": 51,
+        "User": "Ярный Изольда",
+        "Status": "Finished",
+        "Steps": 17629
+    },
+    {
+        "Rank": 52,
+        "User": "Шихранова Ульяна",
+        "Status": "Finished",
+        "Steps": 17499
+    },
+    {
+        "Rank": 53,
+        "User": "Решетников Прокл",
+        "Status": "Finished",
+        "Steps": 17252
+    },
+    {
+        "Rank": 54,
+        "User": "Абрамова Христина",
+        "Status": "Finished",
+        "Steps": 16757
+    },
+    {
+        "Rank": 55,
+        "User": "Калюта Мстислав",
+        "Status": "Finished",
+        "Steps": 15640
+    },
+    {
+        "Rank": 56,
+        "User": "Большая Мария",
+        "Status": "Finished",
+        "Steps": 15358
+    },
+    {
+        "Rank": 57,
+        "User": "Ермишина Эльвира",
+        "Status": "Finished",
+        "Steps": 15306
+    },
+    {
+        "Rank": 58,
+        "User": "Путинцев Степан",
+        "Status": "Finished",
+        "Steps": 14327
+    },
+    {
+        "Rank": 59,
+        "User": "Чюличкова Яна",
+        "Status": "Finished",
+        "Steps": 14094
+    },
+    {
+        "Rank": 60,
+        "User": "Афанасьева Валентина",
+        "Status": "Finished",
+        "Steps": 14024
+    },
+    {
+        "Rank": 61,
+        "User": "Ворожцов Поликарп",
+        "Status": "Finished",
+        "Steps": 13834
+    },
+    {
+        "Rank": 62,
+        "User": "Шихирин Капитон",
+        "Status": "Finished",
+        "Steps": 13578
+    },
+    {
+        "Rank": 63,
+        "User": "Щеголяева Вероника",
+        "Status": "Finished",
+        "Steps": 13426
+    },
+    {
+        "Rank": 64,
+        "User": "Гребенникова Изольда",
+        "Status": "Finished",
+        "Steps": 13409
+    },
+    {
+        "Rank": 65,
+        "User": "Толкачёва Пелагея",
+        "Status": "Finished",
+        "Steps": 13262
+    },
+    {
+        "Rank": 66,
+        "User": "Шурдука Лиана",
+        "Status": "Finished",
+        "Steps": 12354
+    },
+    {
+        "Rank": 67,
+        "User": "Ягужинский Мирослав",
+        "Status": "Finished",
+        "Steps": 11778
+    },
+    {
+        "Rank": 68,
+        "User": "Ледовской Гавриил",
+        "Status": "Finished",
+        "Steps": 11605
+    },
+    {
+        "Rank": 69,
+        "User": "Пашков Агафон",
+        "Status": "Finished",
+        "Steps": 11503
+    },
+    {
+        "Rank": 70,
+        "User": "Спирьянов Порфирий",
+        "Status": "Finished",
+        "Steps": 10717
+    },
+    {
+        "Rank": 71,
+        "User": "Тюшняков Борислав",
+        "Status": "Finished",
+        "Steps": 10405
+    },
+    {
+        "Rank": 72,
+        "User": "Кондурова Эмилия",
+        "Status": "Finished",
+        "Steps": 9827
+    },
+    {
+        "Rank": 73,
+        "User": "Королева Ярослава",
+        "Status": "Finished",
+        "Steps": 9750
+    },
+    {
+        "Rank": 74,
+        "User": "Поветникова Ираида",
+        "Status": "Finished",
+        "Steps": 9358
+    },
+    {
+        "Rank": 75,
+        "User": "Яранов Парфен",
+        "Status": "Finished",
+        "Steps": 9186
+    },
+    {
+        "Rank": 76,
+        "User": "Карюкин Ростислав",
+        "Status": "Finished",
+        "Steps": 8844
+    },
+    {
+        "Rank": 77,
+        "User": "Чукреев Пахом",
+        "Status": "Finished",
+        "Steps": 8531
+    },
+    {
+        "Rank": 78,
+        "User": "Гречко Лев",
+        "Status": "Finished",
+        "Steps": 8107
+    },
+    {
+        "Rank": 79,
+        "User": "Ковшутина Дарья",
+        "Status": "Finished",
+        "Steps": 7836
+    },
+    {
+        "Rank": 80,
+        "User": "Кристина Ляхова",
+        "Status": "Finished",
+        "Steps": 7819
+    },
+    {
+        "Rank": 81,
+        "User": "Мамонов Герасим",
+        "Status": "Finished",
+        "Steps": 7335
+    },
+    {
+        "Rank": 82,
+        "User": "Шапцев Борислав",
+        "Status": "Finished",
+        "Steps": 7102
+    },
+    {
+        "Rank": 83,
+        "User": "Стародубцева Полина",
+        "Status": "Finished",
+        "Steps": 6075
+    },
+    {
+        "Rank": 84,
+        "User": "Иканова Тамара",
+        "Status": "Finished",
+        "Steps": 5639
+    },
+    {
+        "Rank": 85,
+        "User": "Барышникова Елена",
+        "Status": "Finished",
+        "Steps": 4662
+    },
+    {
+        "Rank": 86,
+        "User": "Тоболенко Прокл",
+        "Status": "Finished",
+        "Steps": 4056
+    },
+    {
+        "Rank": 87,
+        "User": "Пыхтина Александра",
+        "Status": "Finished",
+        "Steps": 4021
+    },
+    {
+        "Rank": 88,
+        "User": "Каунайте Мир",
+        "Status": "Finished",
+        "Steps": 3952
+    },
+    {
+        "Rank": 89,
+        "User": "Разнуликов Павел",
+        "Status": "Finished",
+        "Steps": 3891
+    },
+    {
+        "Rank": 90,
+        "User": "Лепихова Галина",
+        "Status": "Finished",
+        "Steps": 2993
+    },
+    {
+        "Rank": 91,
+        "User": "Максимушкин Пимен",
+        "Status": "Finished",
+        "Steps": 2871
+    },
+    {
+        "Rank": 92,
+        "User": "Кружкова Нона",
+        "Status": "Finished",
+        "Steps": 2229
+    },
+    {
+        "Rank": 93,
+        "User": "Плюхин Максим",
+        "Status": "Finished",
+        "Steps": 1912
+    },
+    {
+        "Rank": 94,
+        "User": "Юхтриц Мирослав",
+        "Status": "Finished",
+        "Steps": 1898
+    },
+    {
+        "Rank": 95,
+        "User": "Малкин Владимир",
+        "Status": "Finished",
+        "Steps": 1524
+    },
+    {
+        "Rank": 96,
+        "User": "Чернов Валерьян",
+        "Status": "Finished",
+        "Steps": 1348
+    },
+    {
+        "Rank": 97,
+        "User": "Харьков Вячеслав",
+        "Status": "Finished",
+        "Steps": 1236
+    },
+    {
+        "Rank": 98,
+        "User": "Пищальникова Нина",
+        "Status": "Finished",
+        "Steps": 1007
+    },
+    {
+        "Rank": 99,
+        "User": "Нарастающий Гавр",
+        "Status": "Finished",
+        "Steps": 1000
+    }
+]
+var btCh =null;
+btForm = null;
+csbt = null;
+cslb = null;
+csinput =null;
+var bt = document.createElement('button');
+var inputCheck = null;
+var tb = document.createElement('input');
+var cb = document.createElement('input');
+var ss = document.createElement("datalist");
+
+var listview = document.createElement("datalist");
+listview.id = 'lstView';
+
+
+
+function CheckFormButtonInit() {
+    btCh = document.forms['btCheckForm'];
+    bt.value = "Show";
+    bt.innerHTML = bt.value;
+    bt.name = c[0].name;
+    bt.label = c[0].label;
+    bt.className = 'btCh';
+    bt.elemtype = c[0].elemtype;
+    var bts = document.createElement('button');
+    btCh.appendChild(bt);
+}
+var str =["Str1","Str2","Str3","Str4"];
+//var ss = document.createElement('Option');
+lvForm =null;
+errs = null;
+lists = null;
+function JsonParsings(name){
+   return objs.filter(function(obj) {
+        return obj.User.indexOf(name) > -1;
+    });
+}
+function JsonParsingsNum(num){
+    return objs.filter(
+        function(data){ return data.Rank === num }
+    );
+}
+
+numlu = document.createElement('ul');
+numlu.className = 'divScroll';
+li = document.createElement('ul');
+li.className = 'divScroll';
+
+numerr = document.createElement('h2');
+numerr.elemtype = 'h2';
+numerr.name = 'numErr';
+numerr.className = 'errm';
+
+function numScrollss(num) {
+    navFrom = document.forms['NumScrollView'];
+    mas = JsonParsingsNum(num);
+    if(mas.length!==null) {
+        for (i=0; i< mas.length;i++){
+            li1 = document.createElement('li');
+            li1.elemtype = 'li';
+            li1.size = 12;
+            li1.innerHTML ="User: " + mas[i].User + "| Number " + mas[i].Rank + "| Status " + mas[i].Status;
+            numlu.appendChild(li1);
+        }
+    }
+    else{
+        numerr.innerHTML = " Not found!";
+        navFrom.appendChild(numerr);
+        return false;
+    }
+    navFrom.appendChild(numlu);
+    return false;
+}
+
+function numButFun() {
+    numView = document.forms['numView'];
+    numlb = document.createElement('h2');
+    numlb.innerHTML = 'Get customer by number ';
+    numlb.className = 'csLable';
+    numView.appendChild(numlb);
+    numbtForm = document.forms['numBut'];
+    numinput = document.createElement('input');
+    numinput.label=c[1].label;
+    numinput.name= 'numInput';
+    numinput.elemtype = c[1].elemtype;
+    numinput.className = 'csInput';
+    numbt = document.createElement('Button');
+    numbt.name = 'numButton';
+    numbt.elemtype = 'button';
+    numbt.className = 'btCs';
+    numbt.innerHTML = 'Show';
+    numbt.label = 'numButton';
+    numbtForm.appendChild(numinput);
+    numbtForm.appendChild(numbt);
+    numbt.onclick = function () {
+        if (numinput.value === "") {
+            numerr.value = 'Input something';
+            numerr.innerHTML = numerr.value;
+            numbtForm.appendChild(numerr);
+            return false;
+        }
+        else{
+            numScrollss(parseInt(numinput.value));
+            return false;
+        }
+    };
+}
+
+
+
+function Scrollss(name) {
+    navFrom = document.forms['ScrollView'];
+    mas = JsonParsings(name);
+    if(mas.length!==null) {
+        for (i=0; i< mas.length;i++){
+            li1 = document.createElement('li');
+            li1.elemtype = 'li';
+            li1.size = 12;
+            li1.innerHTML ="User: " + mas[i].User + "| Number " + mas[i].Rank + "| Status " + mas[i].Status;
+            li.appendChild(li1);
+        }
+    }
+    else{
+        cserr.innerHTML = " Not found!";
+        navFrom.appendChild(cserr);
+        return false;
+    }
+    navFrom.appendChild(li);
+    return false;
+}
+
+
+function fform(){
+    lvForm =  document.forms['listView'];
+    lists = document.createElement('select');
+    errs = document.createElement('text');
+    lists.elemtype = 'select';
+    lists.name = 'select';
+    listview.className = 'lists';
+    errs.className = 'errm';
+    errs.elemtype = 'text';
+
+}
+function fillopt(){
+    for (var i = 0; i < str.length; ++i) {
+        // Append the element to the end of Array list
+        lists[lists.length] = new Option(str[i]);
+    }
+}
+bt.onclick = function () {
+    if(cb.checked){
+        errs.innerHTML ="";
+        fillopt();
+        lvForm.appendChild(lists);
+        return false;
+    }
+    else if (tb.value === "") {
+        errs.innerHTML = 'Input something';
+        lvForm.appendChild(errs);
+        return false;
+    }
+    else if(str.includes(tb.value)){
+            lists.options.namedItem(tb.text);
+            return false;
+        }
+    else{
+            errs.innerHTML = "check not found!";
+            lvForm.appendChild(errs);
+            return false;
+    }
+};
+cserr = document.createElement('h2');
+cserr.elemtype = 'h2';
+cserr.name = 'csErr';
+cserr.className = 'errm';
+function csButFun() {
+    csView = document.forms['csView'];
+    cslb = document.createElement('h2');
+    cslb.innerHTML = 'Get customer by Name ';
+    cslb.className = 'csLable';
+    csView.appendChild(cslb);
+    btForm = document.forms['csBut'];
+    csinput = document.createElement('input');
+    csinput.label=c[1].label;
+    csinput.name= 'csInput';
+    csinput.elemtype = c[1].elemtype;
+    csinput.className = 'csInput';
+    csbt = document.createElement('Button');
+    csbt.name = 'csButton';
+    csbt.elemtype = 'button';
+    csbt.className = 'btCs';
+    csbt.innerHTML = 'Show';
+    csbt.label = 'csButton';
+    btForm.appendChild(csinput);
+    btForm.appendChild(csbt);
+    csbt.onclick = function () {
+        if (csinput.value === "") {
+            cserr.value = 'Input something';
+            cserr.innerHTML = cserr.value;
+            btForm.appendChild(cserr);
+            return false;
+        }
+        else{
+            Scrollss(csinput.value);
+            return false;
+        }
+    };
+}
+function CheckFormInputInit() {
+    inputCheck = document.forms['tbForm'];
+    tb.name = c[1].name;
+    tb.elemtype = c[1].elemtype;
+    tb.className = c[1].className;
+    tb.txt = "";
+    inputCheck.appendChild(tb);
+    cb.name = c[2].name;
+    cb.type = c[2].elemtype;
+    cb.className = c[2].className;
+    inputCheck.appendChild(cb);
+}
